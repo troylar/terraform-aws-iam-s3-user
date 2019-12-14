0a4197ea7453606b5a34765e1e7e2bab24097774
Troy Larson <1931732+troylar@users.noreply.github.com>
2019-12-14 08:04:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 data "aws_iam_policy_document" "default" {
   count = var.enabled ? 1 : 0
+  
+  statement {
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
 
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+  
   statement {
     actions   = var.s3_actions
     resources = var.s3_resources
